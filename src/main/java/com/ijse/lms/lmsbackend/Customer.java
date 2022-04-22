5792180b85c6e49b30352a312d23ebc31bd40f97
Gihara Wickramatunge <giharanw@gmail.com>
2022-04-22 11:28:03 +0530

    Add JSONB to the project

---
+++ src/main/java/com/ijse/lms/lmsbackend/Customer.java
@@ -0,0 +1,55 @@
+package com.ijse.lms.lmsbackend;
+
+import jakarta.json.bind.annotation.JsonbProperty;
+
+public class Customer {
+
+    private String name;
+    private String nic;
+    private String email;-
+
+    public Customer(String nic, String name, String email) {
+        this.nic = nic;
+        this.name = name;
+        this.email = email;
+        System.out.println("Customer() - full arg");
+    }
+
+    public Customer() {
+        System.out.println("Customer() - no arg");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "nic='" + nic + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/ijse/lms/lmsbackend/StudentServlet.java
+++ src/main/java/com/ijse/lms/lmsbackend/StudentServlet.java
@@ -1,8 +1,15 @@
 package com.ijse.lms.lmsbackend;
 
+import jakarta.json.*;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentServlet extends HttpServlet {
     @Override
@@ -12,18 +19,48 @@ public class StudentServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getContentType()==null || !request.getContentType().equalsIgnoreCase("application/json")){
+        if (request.getContentType()==null || !request.getContentType().toLowerCase().startsWith("application/json")){
             response.sendError(415);
+            return;
         }
+//        BufferedReader reader = request.getReader();
+//        StringBuilder sb = new StringBuilder();
+//        reader.lines().forEach(line -> sb.append(line+"\n"));
+//        System.out.println(sb.toString());
+//
+//        JsonReader jsonReader = Json.createReader(request.getReader());
+//        JsonArray array = jsonReader.readArray();
+//        for (int i = 0; i < array.size(); i++) {
+//            JsonObject elm = array.getJsonObject(i);
+//            System.out.println(elm.getString("name"));
+//            System.out.println(elm.getString("nic"));
+//            System.out.println(elm.getString("email"));
+//        }
+//
+//        response.setContentType("application/json");
+//        JsonWriter writer = Json.createWriter(response.getWriter());
+//        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+//        arrayBuilder.add(10);
+//        arrayBuilder.add(false);
+//        arrayBuilder.add(Json.createArrayBuilder().add(10).add(20).build());
+//        arrayBuilder.add(Json.createObjectBuilder().add("id","C001").add("name","Gihara").build());
+//        JsonArray jsonArray = arrayBuilder.build();
+//        writer.writeArray(jsonArray);
+        Jsonb jsonb = JsonbBuilder.create();
+        List<Customer> customerList = jsonb.fromJson(request.getReader(),
+                new ArrayList<Customer>(){}.getClass().getGenericSuperclass());
+
+        customerList.forEach(System.out::println);
+
+        List<Customer> anotherCustomerList = new ArrayList<>();
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+
+        response.setContentType("application/json");
+        jsonb.toJson(anotherCustomerList, response.getWriter());
     }
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
 }
